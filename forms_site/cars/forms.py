5d8a1fc5a8d5d3166f50099c2223698eaf794c3c
Steve <stephenjmhough@outlook.com>
2025-08-13 11:44:29 +0100

    added the model form and added save functionality for the feedback

--- forms_site/cars/forms.py
+++ forms_site/cars/forms.py
@@ -1,7 +1,14 @@
 from django import forms
+from .models import Review
+from django.forms import ModelForm
 
-class ReviewForm(forms.Form):
-    first_name = forms.CharField(label='First Name', max_length=30)
-    last_name = forms.CharField(label='Last Name', max_length=30)
-    email = forms.EmailField(label='Email')
-    review = forms.CharField(label='Please write your review here.')
+# class ReviewForm(forms.Form):
+#     first_name = forms.CharField(label='First Name', max_length=30)
+#     last_name = forms.CharField(label='Last Name', max_length=30)
+#     email = forms.EmailField(label='Email')
+#     review = forms.CharField(label='Please write your review here.', widget=forms.Textarea(attrs={'class':'myform'}))
+
+class ReviewForm(ModelForm):
+    class Meta:
+        model = Review
+        fields = ['first_name', 'last_name', 'stars']
