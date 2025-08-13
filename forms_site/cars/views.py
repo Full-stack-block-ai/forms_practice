3988e410aea2f9fb75598cfb8197115848a05982
Steve <stephenjmhough@outlook.com>
2025-08-13 09:56:10 +0100

    setup a basic feedback page that prints out the data and redirects to a thank you

--- forms_site/cars/views.py
+++ forms_site/cars/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import ReviewForm
+from django.urls import reverse
 
 # Create your views here.
+def rental_review(request):
+
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            return redirect(reverse('cars:thank_you'))
+
+    else:
+        form = ReviewForm()
+    return render(request, 'cars/rentalreview.html', context={'form': form})
+    
+
+    
+
 def thank_you(request):
     return render(request, 'cars/thankyou.html')
 
-def rental_review(request):
-    return render(request, 'cars/rentalreview.html')
